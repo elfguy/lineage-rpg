440827b56bb81f0ffb0986d3d2c376c963b047cf
mini-pc <elfguy81@gmail.com>
2026-03-27 01:45:43 +0900

    feat: 경험치 & 레벨업 시스템 (이슈 #28)
    - GameState.add_experience() 함수
    - 레벨업 시 스탯 증가 (HP+10, MP+5, ATK+2, DEF+1)
    - 경험치 요구량 1.5배 증가
    - 레벨업 이벤트 발생
    
    Refs: #28

--- source/systems/game_state.gd
+++ source/systems/game_state.gd
@@ -15,3 +15,30 @@ var player_data: Dictionary = {
 	"defense": 5,
 	"gold": 0,
 }
+
+func add_experience(amount: int) -> void:
+	"""경험치 추가 및 레벨업 체크"""
+	player_data["experience"] += amount
+	EventBus.experience_gained.emit(amount)
+
+	# 레벨업 체크
+	while player_data["experience"] >= player_data["experience_to_next"]:
+		_level_up()
+
+func _level_up() -> void:
+	"""레벨업 처리"""
+	player_data["experience"] -= player_data["experience_to_next"]
+	player_data["level"] += 1
+	player_data["experience_to_next"] = int(player_data["experience_to_next"] * 1.5)
+
+	# 스탯 증가
+	player_data["max_hp"] += 10
+	player_data["hp"] = player_data["max_hp"]  # HP 회복
+	player_data["max_mp"] += 5
+	player_data["mp"] = player_data["max_mp"]
+	player_data["attack"] += 2
+	player_data["defense"] += 1
+
+	EventBus.level_up.emit(player_data["level"])
+	EventBus.health_changed.emit(player_data["hp"], player_data["max_hp"])
+
